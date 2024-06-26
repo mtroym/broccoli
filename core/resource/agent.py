fd72c35406e04b09d9ead6da32dac0e98d0b7720
0x7 <maoym.encryp@gmail.com>
2024-06-26 21:16:11 +0800

    sync

---
+++ core/resource/agent.py
@@ -0,0 +1,20 @@
+class Agent(object):
+    
+    # thread safe Agent both interact with web3 and web2.
+    def __init__(self, name: str) -> None:
+        self.name = name
+        # web3
+        self.account = None
+        self.slave_accounts = []
+        
+        # web2
+        self.headers = {}
+        self.discord = None
+        self.twitter = None
+        
+    def export_account(self, verbose=False):
+        return {}        
+        
+        
+    def restore_account(self, verbose=False):
+        return None
--- core/resource/contract.py
+++ core/resource/contract.py
@@ -1,17 +1,18 @@
 from web3.contract.contract import Contract
 
+
 def make_contract(contract_conf: dict):
     if "network" not in contract_conf or \
         "contract_address" not in contract_conf or \
-        "abis" not in contract_conf:
+            "abis" not in contract_conf:
         raise ValueError("contract config not complete")
-    
-    return 
+
+    return
 
 
 class ContractWarpper(object):
     def __init__(self, contract_conf: dict) -> None:
         self.network, self.contract_address, self.abis = \
             make_contract(contract_conf=contract_conf)
-        
+
         self.contract = Contract(address=self.contract_address, abis=self.abis)
